0d52be2ef0c328e3df3d56fe107e9021a64ab6fa
blt_r <www.edav@gmail.com>
2025-10-27 02:18:23 +0400

    Only register service-worker in production

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
+  import { dev } from "$app/environment";
   import "../app.css";
+
+  $effect(() => {
+    if (!dev && navigator.serviceWorker) {
+      navigator.serviceWorker.register("/service-worker.js");
+    }
+  });
 
   let { children } = $props();
 </script>

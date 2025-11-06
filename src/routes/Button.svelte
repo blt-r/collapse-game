503c38d98adf63f41804190abbec499d786ea5f5
blt_r <www.edav@gmail.com>
2025-11-07 00:49:16 +0400

    Add button click animation

--- src/routes/Button.svelte
+++ src/routes/Button.svelte
@@ -22,9 +22,10 @@
     onclick?.(e);
   }}
   class={[
-    "block cursor-pointer bg-gray-200",
-    "disabled:cursor-default disabled:opacity-30",
-    "transition-colors duration-150",
+    "block bg-gray-200 enabled:cursor-pointer",
+    "disabled:bg-gray-100 disabled:text-gray-400",
+    "transition-[background_scale] duration-75",
+    "active:enabled:scale-95 active:enabled:bg-gray-300",
     size === "small" && "rounded-[.7cqw] p-[.75cqw] [&>svg]:size-[2.1cqw]",
     size === "big" && "rounded-[1cqw] p-[1.2cqw] [&>svg]:size-[4.1cqw]",
     cls,
